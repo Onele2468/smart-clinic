8047f4b7ded06ab69ab52b9dba914fe16d00051d
Onele2468 <onelebulwana2@gmail.com>
2026-06-05 21:41:12 +0200

    Enable esModuleInterop for Vercel

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -15,14 +15,14 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: Request) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: Response) {
         return {
           statusCode: res.statusCode,
         };
